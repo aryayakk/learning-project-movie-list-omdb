b16c218a28e1a36b46b6aa55d91ff073d958c1d5
aryayakk <nonebutarya@gmail.com>
2022-01-28 10:24:58 +0700

    Fungsi Modal bekerja dengan baik

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,56 +1,45 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const MovieList = () => {
-  const [dataMovie, setDataMovie] = useState([
-    {
-      "Title": "Money Heist",
-      "Year": "2017–2021",
-      "imdbID": "tt6468322",
-      "Type": "series",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDJkYzY3MzMtMGFhYi00MmQ4LWJkNTgtZGNiZWZmMTMxNzdlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg"
-    },
-    {
-      "Title": "Black Mirror: Bandersnatch",
-      "Year": "2018",
-      "imdbID": "tt9495224",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjM5MzgzMjM3OF5BMl5BanBnXkFtZTgwMzQ2MzQwNzM@._V1_SX300.jpg"
-    },
-    {
-    "Title": "The Silent Sea",
-    "Year": "2021–",
-    "imdbID": "tt11570202",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjFkNDNjYTktZjJiNi00Y2U3LWE2NDgtZDM3YjIzMWUzNGM2XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg"
-    },
-  ]);
+  const [dataMovie, setDataMovie] = useState([]);
+  const [dataDetail, setDetail] = useState([]);
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const getFrom = await axios.get('http://www.omdbapi.com/?apikey=[a477174f]&')
-  //     .then((res) => getFrom);
+  useEffect(() => {
+    const getData = async () => {
+      const getFrom = await axios.get('http://www.omdbapi.com/?apikey=a477174f&s=Avengers')
+      const result = getFrom.data.Search;
 
-  //     setDataMovie()
-  //   }
+      setDataMovie(result);
+    }
 
-  //   getData()
-  // }, [])
+    getData()
+  }, [])
 console.log('dataMovie', dataMovie);
+
+  const showDetail = async (e) => {
+    const clicked = e.target.value;
+    const show = await axios.get(`http://www.omdbapi.com/?apikey=a477174f&i=${clicked}`)
+    console.log('show', show);
+    setDetail(show.data)
+  }
+
   return (
     <>
       <div className="container">
-        <div className="row">
+        <div className="row mt-5">
+          <h1 className="text-warning">Jajal Movie DB</h1>
           {
             dataMovie
-              ? dataMovie.map((res, imdbID) => (
-                <div className="col-md-4 my-4">
-                  <div className="card" key={imdbID} >
-                  <img src={res.Poster} className="card-img-top" alt=""/>
+              ? dataMovie.map((res) => (
+                <div className="col-md-4 my-4" key={res.imdbID}>
+                  <div className="card">
+                  <img src={res.Poster} className="card-img-top" alt="" />
                     <div className="card-body">
                       <h5 className="card-title">{res.Title}</h5>
                       <h6 className="card-subtitle mb-2 text-muted">{res.Year}</h6>
-                      <button href="#" className="btn btn-primary">Show Details</button>
+                      <button href="#" className="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" onClick={showDetail} value={res.imdbID}>Show Details</button>
                     </div>
                   </div>
                 </div>
@@ -58,7 +47,41 @@ console.log('dataMovie', dataMovie);
             : false
           }
         </div>  
-      </div>    
+      </div>
+
+      {/* MODAL */}
+      <div className="modal fade" id="movieDetailModal" tabIndex="-1" aria-labelledby="movieDetailModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="movieDetailModalLabel">Modal title</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-3">
+                    <img src="" className="img-fluid" />
+                  </div>
+
+                  <div className="col-md">
+                    <ul className="list-group">
+                      <li className="list-group-item"><h4>{dataDetail.Title} {dataDetail.Year}</h4></li>
+                      <li className="list-group-item"><strong>Director : </strong>{dataDetail.Director}</li>
+                      <li className="list-group-item"><strong>Actors : </strong>{dataDetail.Actors}</li>
+                      <li className="list-group-item"><strong>Writer : </strong>{dataDetail.Writer}</li>
+                      <li className="list-group-item"><strong>Plot : </strong>{dataDetail.Plot}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,25 +4,25 @@ import React from 'react';
 function Navbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <div class="container">
-          <a class="navbar-brand" href="#">Jajal Movie</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <div className="container">
+          <a className="navbar-brand" href="#">Jajal Movie</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Collection</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Collection</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact Us</a>
               </li>
             </ul>
           </div>
